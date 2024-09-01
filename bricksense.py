fe7d56262cd05e7cbce037590ad360d75e392975
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-01 08:40:05 +0500

    Update bricksense.py

--- bricksense.py
+++ bricksense.py
@@ -186,9 +186,10 @@ else:
                     # Capitalize each class name individually
                     st.write(f"YOLOv5 detected the following classes with high confidence: {', '.join([name.capitalize() for name in yolo_detected_classes])}")
         
-            # Handle case where yolo_results is None or empty
-            if not yolo_detected_classes:
-                # st.write("No classes detected with high confidence using YOLOv5.")
+        # Handle case where yolo_results is None or empty
+        if not yolo_detected_classes:
+            # st.write("No classes detected with high confidence using YOLOv5.")
+            pass
         
         # Step 2: ImageNet classification
         imagenet_predictions = import_and_predict_imagenet(image, imagenet_model)
