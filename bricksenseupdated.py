7e4e9b6444ecec7d9d703abf08e54cea2d3dd788
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-25 17:38:26 +0500

    Update bricksenseupdated.py

--- bricksenseupdated.py
+++ bricksenseupdated.py
@@ -330,14 +330,17 @@ else:
                 """, unsafe_allow_html=True)
 
                 # st.write("")  # Creates a blank line
+                # Default sensitivity Selection
                 sensitivity=9
+                layername= model.layers[sensitivity].name
+                layershape= model.layers[sensitivity].output.shape
                 # Display Layer info for selected model in two columns
                 col1, col2 = st.columns(2)
                 with col1:
-                    st.write(f"Layer Name: ",model.layers[sensitivity].name)
+                    st.write(f"Layer Name: ",layername)
                 
                 with col2:
-                    st.write(f"Layer Shape: ",model.layers[sensitivity].output.shape)
+                    st.write(f"Layer Shape: ",layershape)
 
                 # Create an expander for sensitivity adjustment
                 with st.expander("🔍 Sensitivity Settings"):
@@ -350,8 +353,9 @@ else:
                         step=1,        # Step for incremental changes
                         format="%.1f"    # Format to display sensitivity with one decimal
                                             )
-                           
-                
+                # layers name based on slider  
+                layername= model.layers[sensitivity].name
+                layershape= model.layers[sensitivity].output.shape
 
                 # Perform prediction again
                 # predictions, image_with_border, contours_with_border, heatmap_image, contoured_image, overlay_img  = import_and_predict(image, sensitivity=sensitivity)
