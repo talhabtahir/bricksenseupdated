2b624cfa5c672e77dff6525a3c8435bf401ae7b9
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-26 23:07:59 +0500

    Update bricksenseupdated.py

--- bricksenseupdated.py
+++ bricksenseupdated.py
@@ -304,9 +304,9 @@ else:
         
             # Define the models to compare
             model_names = [
-                "230kmodelv11_version_cam_2",
-                "230kmodelv12_version_cam_2",
-                "230kmodelv13_version_cam_3",
+                # "230kmodelv11_version_cam_2",
+                # "230kmodelv12_version_cam_2",
+                # "230kmodelv13_version_cam_3",
                 "230kmodelv141_version_cam_3",
                 "230kmodelv142_version_cam_3",
                 "230kmodelv143_version_cam_3"
