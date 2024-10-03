41c127ce3f25046e90f34a7ca7ceb93beeb9aebe
Talha Bin Tahir <talhabtahir@gmail.com>
2024-10-03 07:32:12 +0500

    Update bricksenseupdated.py

--- bricksenseupdated.py
+++ bricksenseupdated.py
@@ -417,9 +417,10 @@ else:
                 "230kmodelv12_version_cam_2",
                 # "230kmodelv13_version_cam_3",
                 # "230kmodelv141_version_cam_3",
-                # "230kmodelv142_version_cam_3",
+                "230kmodelv142_version_cam_3",
                 "230kmodelv143_version_cam_3",
-                "230kmodelv151_version_cam_3"
+                "230kmodelv151_version_cam_3",
+                "230kmodelv17_version_cam_3"
                 
             ]
         
@@ -505,22 +506,22 @@ else:
                 
             # st.image(overlay_images[0], caption=f"{model_names[0]}", use_column_width=True)
             # # Display overlay images for each model in two columns
-            col1, col2= st.columns(2)
+            col1, col2, col3= st.columns(3)
         
             with col1:
                 st.image(overlay_images[0], caption=f"{model_names[0]}", use_column_width=True)
-                st.image(overlay_images[2], caption=f"{model_names[2]}", use_column_width=True)
+                st.image(overlay_images[4], caption=f"{model_names[4]}", use_column_width=True)
                 # st.write(len(model.layers))  # Creates a blank line
 
             
             with col2:
                 st.image(overlay_images[1], caption=f"{model_names[1]}", use_column_width=True)
-                st.image(overlay_images[3], caption=f"{model_names[3]}", use_column_width=True)
+                st.image(overlay_images[5], caption=f"{model_names[5]}", use_column_width=True)
                 # st.write("")  # Creates a blank line
 
-            # with col3:
-            #     st.image(overlay_images[2], caption=f"{model_names[2]}", use_column_width=True)
-            #     # st.image(overlay_images[5], caption=f"{model_names[5]}", use_column_width=True)
+            with col3:
+                st.image(overlay_images[3], caption=f"{model_names[3]}", use_column_width=True)
+                st.image(overlay_images[6], caption=f"{model_names[6]}", use_column_width=True)
             #     st.write("")  # Creates a blank line
 
         except Exception as e:
