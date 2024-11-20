85f5120c365016be0d27f72ec27f3ea4df7eea05
Talha Bin Tahir <talhabtahir@gmail.com>
2024-11-20 11:24:37 +0500

    Update bricksenseupdated.py

--- bricksenseupdated.py
+++ bricksenseupdated.py
@@ -19,13 +19,6 @@ st.set_page_config(
     layout="centered"
 )
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 # Display logo instead of header
 imagelogo = Image.open("static/sidelogo2.png")
